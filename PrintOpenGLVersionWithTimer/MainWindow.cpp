bc7357f49244074b6993c03d19ece6e475cb1ce0
ngzHappy <819869472@qq.com>
2016-01-21 14:42:56 +0800

    +opengl version

--- PrintOpenGLVersionWithTimer/MainWindow.cpp
+++ PrintOpenGLVersionWithTimer/MainWindow.cpp
@@ -30,11 +30,20 @@ void MainWindow::resizeGL(int w, int h) {glViewport(0,0,w,h);}
 
 void MainWindow::timerEvent(QTimerEvent *e) {
     QGLWidget::timerEvent(e);
+
     qDebug()
         <<"opengl version:"
         <<format().majorVersion()
         <<format().minorVersion()
         <<(thisData->timerStamp++);
+
+    qDebug().noquote()<<QString::fromUtf8( reinterpret_cast<const char *>(glGetString(GL_VERSION)));
+
+    GLint major,minor;
+    glGetIntegerv(GL_MAJOR_VERSION,&major);
+    glGetIntegerv(GL_MINOR_VERSION,&minor);
+    qDebug().noquote()<<major<<minor;
+
 }
 
 
